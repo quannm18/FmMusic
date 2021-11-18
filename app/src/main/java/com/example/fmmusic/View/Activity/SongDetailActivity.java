12c8bae1a99faa89869d55f2fcef8c2c778e9b08
ngomi <ngominhquan20021809@gmail.com>
2021-11-18 15:56:24 +0700

    update 1.0

---
+++ app/src/main/java/com/example/fmmusic/View/Activity/SongDetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.fmmusic.View.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fmmusic.R;
+
+public class SongDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_song_detail);
+    }
+}
