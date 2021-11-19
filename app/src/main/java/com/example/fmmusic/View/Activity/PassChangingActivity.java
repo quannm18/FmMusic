ced9f3df9e28bbb74dd62839ddadb353ac2e4c3b
hoainambeco <namxg1@gmail.com>
2021-11-19 08:21:03 +0700

    done activity_pass_changing.xml

--- app/src/main/java/com/example/fmmusic/View/Activity/PassChangingActivity.java
+++ app/src/main/java/com/example/fmmusic/View/Activity/PassChangingActivity.java
@@ -11,17 +11,9 @@ import com.example.fmmusic.R;
 
 public class PassChangingActivity extends AppCompatActivity {
 //Nguyễn Hoài Nam đảm nhận thằng này
-    TextView tvFocusedPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pass_changing);
-        tvFocusedPass = findViewById(R.id.tvFocusedPass);
-        tvFocusedPass.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Focused Password",Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 }
