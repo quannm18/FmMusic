bfe2b94b500012ed6fd8f1bc1cf78d42ab949c13
Ngo Minh Quan <ngominhquan20021809@gmail.com>
2021-12-01 18:37:54 +0700

    add new SingerHome then changepass

--- app/src/main/java/com/example/fmmusic/View/Activity/DownLoadActivity.java
+++ app/src/main/java/com/example/fmmusic/View/Activity/DownLoadActivity.java
@@ -2,6 +2,7 @@ package com.example.fmmusic.View.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.app.DownloadManager;
 import android.content.Intent;
 import android.net.Uri;
@@ -24,5 +25,6 @@ public class DownLoadActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String id = intent.getStringExtra("id");
         webViewDownload.loadUrl("http://api.mp3.zing.vn/api/streaming/audio/"+id+"/320");
+        finish();
     }
 }
